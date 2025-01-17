dce7fb8a3cb51f4469766f52f2dfeaa6a82046dc
SenbiDev <senbidev@gmail.com>
2025-01-17 10:11:27 +0700

    modif: add dummy token at fetcher file

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -6,7 +6,8 @@ const fetcher = axios.create({
   baseURL: "/api",
 });
 
-if (myCookie) {
+fetcher.defaults.headers.common["Authorization"] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiMTgzNGRjLTk3ZDktNDI0MC05MGYzLTU5YzI1YzA2MTA2YSIsInVzZXJfbmFtZSI6ImN1eXkiLCJlbWFpbCI6ImZhZGhpbEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCR4eU1IYkVucFJpZk9oTjh0ckQuZU9lWGppN2NYa3FXeFZnMU5Ua1lEL0RSaE5rNnlQZUxFcSIsImlzX2FkbWluIjpmYWxzZSwiY3JlYXRlZF9hdCI6IjIwMjUtMDEtMTFUMDQ6NDQ6MzcuNDY4WiIsInVwZGF0ZV9hdCI6IjIwMjUtMDEtMTFUMDQ6NDQ6MzcuNDY4WiIsImlhdCI6MTczNzAwMzk3MywiZXhwIjoxNzM5NDIzMTczfQ.DNjzfmdblfD54FqCKMTqb6WOFBnk7DkipQsZYJ3qRtQ`;
+if (myCookie) {  
   fetcher.defaults.headers.common["Authorization"] = `Bearer ${myCookie}`;
 }
 // request.po
